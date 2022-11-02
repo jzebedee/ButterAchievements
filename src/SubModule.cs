8306a7e792d4b7d7ce3c3d210f6f37bf69b951d5
J. Zebedee <zebedee@code.gripe>
2022-11-02 16:14:21 -0500

    Add some helpful comments

--- src/SubModule.cs
+++ src/SubModule.cs
@@ -15,6 +15,19 @@ namespace ButterAchievements
         {
             base.OnSubModuleLoad();
 
+            //EnableAchievementsPatch:
+            // 1. re-enables achievements in previously tainted savefiles
+            // 2. activates achievements even if mods or cheat mode is present
+
+            //EnableSandboxAchievementsPatch:
+            // 1. loads the achievements behavior in Sandbox mode games
+
+            //SwallowStoryModeAchievementsDuringSandboxPatch:
+            // 1. prevents game from crashing while registering Story mode achievements that Sandbox doesn't have
+
+            //SuppressModulesPatch:
+            // 1. hides non-official mods from the module list, so that tainted saves can be used in vanilla and after using this mod
+
             _harmony.Value.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
