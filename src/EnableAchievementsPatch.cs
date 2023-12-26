035dc2b58c51876895e5b78b447f6abf4f44bf34
J. Zebedee <zebedee@code.gripe>
2023-12-25 22:38:05 -0600

    Use nameof for target method in EnableAchievementsPatch

--- src/EnableAchievementsPatch.cs
+++ src/EnableAchievementsPatch.cs
@@ -2,18 +2,21 @@
 
 namespace ButterAchievements;
 
-    //StoryMode.GameComponents.CampaignBehaviors.AchievementsCampaignBehavior.CheckAchievementSystemActivity() : bool @060003DE
-    //// Token: 0x060003DE RID: 990 RVA: 0x00017B98 File Offset: 0x00015D98
-    [HarmonyPatch(typeof(StoryMode.GameComponents.CampaignBehaviors.AchievementsCampaignBehavior), "CheckAchievementSystemActivity")]
-    public class EnableAchievementsPatch
+//StoryMode.GameComponents.CampaignBehaviors.AchievementsCampaignBehavior.CheckAchievementSystemActivity() : bool @060003DE
+// Token: 0x060003DE RID: 990 RVA: 0x00017B98 File Offset: 0x00015D98
+[HarmonyPatch(
+    typeof(StoryMode.GameComponents.CampaignBehaviors.AchievementsCampaignBehavior),
+    nameof(StoryMode.GameComponents.CampaignBehaviors.AchievementsCampaignBehavior.CheckAchievementSystemActivity)
+)]
+public class EnableAchievementsPatch
+{
+    public static bool Prefix(ref bool __result, ref bool ____deactivateAchievements)
     {
-        public static bool Prefix(ref bool __result, ref bool ____deactivateAchievements)
-        {
-            //don't deactivate achievements
-            ____deactivateAchievements = false;
-            //do pass achievement system check
-            __result = true;
-            //don't run original
-            return false;
-        }
+        //don't deactivate achievements
+        ____deactivateAchievements = false;
+        //do pass achievement system check
+        __result = true;
+        //don't run original
+        return false;
     }
+}
